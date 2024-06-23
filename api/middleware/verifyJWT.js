d03092fd2046ff52008025743fa90326dd1f82a3
Sunny Roy <er.sunnyroy@gmail.com>
2024-06-23 14:52:01 +0530

    JWT Verify Middleware introduced

--- api/middleware/verifyJWT.js
+++ api/middleware/verifyJWT.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken')
 require('dotenv').config();
+const jwt = require('jsonwebtoken')
 
 const verifyJWT = (req, res, next) =>{
     const authHeader = req.headers.authorization || req.headers.Authorization;
